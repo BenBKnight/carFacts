dac988a930e866d32342bda4fce4dc86b3298561
Jordan Johnson <jordaninjohnson@gmail.com>
2020-06-08 20:11:44 -0600

    commit

--- public/js/members.js
+++ public/js/members.js
@@ -9,4 +9,14 @@ const logoutBtn = $(".logoutBtn");
 logoutBtn.on("click", event => {
   event.preventDefault();
   window.location.replace("/logout");
+
+  // CLick Listener for vehicle button
+  const vehicleBtn = $("#vehicleButton");
+  vehicleBtn.on("click", () => {
+    console.log("button clicked");
+    window.location.replace("/vehicles");
+    // If there's an error, handle it by throwing up a bootstrap alert
+    console.log("changed pages");
+  })
+    .catch(handleLoginErr);
 });
