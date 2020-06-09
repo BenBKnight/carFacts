c6310e45ae830e5cd7a35464904962a349c73a93
george-g <georgegombert@gmail.com>
2020-06-08 18:58:17 -0600

    added api routes

--- routes/maintenance.js
+++ routes/maintenance.js
@@ -17,4 +17,42 @@ router.get("/maintenance/maintRecord", (req, res) => {
   res.render("maintRecord");
 });
 
+router.post("/api/maintenance", (req, res) => {
+  db.Maintenance
+    .create({
+      name: req.body.name,
+      description: req.body.description,
+      milage: req.body.milage,
+      parts: req.body.parts,
+      jobDate: req.body.jobDate,
+      vehicle: req.body.vehicle,
+    })
+    .then(() => {
+      res.status(200).end();
+    })
+    .catch(err => {
+      res.status(401).json(err);
+    });
+});
+
+router.get("/api/maintenance", (req, res) => {
+  db.Maintenance
+    .findAll()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch(() => res.status(401).json(err));
+});
+
+router.get("/maintenance/:jobid", (req, res) => {
+  const jobId = req.params.jobid;
+  db.Maintenance
+    .findAll({
+      where: {
+        id : jobId
+      }
+    })
+    .then(result => res.json(result))
+});
+
 module.exports = router;
