5931ef7fa1fea8ba1c56946c06419dee3d5e45ae
george-g <georgegombert@gmail.com>
2020-06-08 19:06:13 -0600

    old html commented out

--- routes/maintenance.js
+++ routes/maintenance.js
@@ -3,17 +3,17 @@ const router = app.Router();
 const passport = require("../config/passport");
 
 router.get("/maintenance", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/maintenance.html"));
+  // res.sendFile(path.join(__dirname, "../public/maintenance.html"));
   res.render("maintenance");
 });
 
 router.get("/newMaintenance", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/newMaintenance.html"));
+  // res.sendFile(path.join(__dirname, "../public/newMaintenance.html"));
   res.render("newMaintenance");
 });
 
 router.get("/maintenance/maintRecord", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/maintRecord.html"));
+  // res.sendFile(path.join(__dirname, "../public/maintRecord.html"));
   res.render("maintRecord");
 });
 
