dcc6d988d53a7d655abfaaa6fb0536cbb3037fdb
george-g <georgegombert@gmail.com>
2020-06-08 19:02:06 -0600

    converted js file

---
+++ public/js/maintenance.js
@@ -0,0 +1,22 @@
+$(document).ready(() => {
+  const jobNames = $("#jobNames");
+  let nameArray = [];
+
+  $.ajax({
+    url: "/api/maintenance",
+    type: "GET",
+  })
+  .then(result => {
+    // nameArray.push(result);
+    
+    result.forEach(index => {
+      jobNames.append(`<li data-jobid= "${index.id}">${index.name}</li>`)
+    });
+  
+  })
+  .catch(err => console.log(err));
+
+  $("ul").click(event => {
+    location.pathname = `/maintenance/${event.target.dataset.jobid}`
+  });
+}); //end document.ready
