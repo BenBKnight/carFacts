f29b98ee51d15a08ee7eed1c071fcc162e893a05
george-g <georgegombert@gmail.com>
2020-06-08 19:16:24 -0600

    js file converted

---
+++ public/js/newMaintenance.js
@@ -0,0 +1,28 @@
+$(document).ready(() => {
+  $("#submit").click(async function() {
+    try {
+      event.preventDefault();
+
+      // Gathering information from the form
+      const maint = {
+        name: $("#jobName").val().trim(),
+        description: $("#jobDescription").val().trim(),
+        milage: parseInt($("#milage").val().trim()),
+        parts: $("#parts").val().trim(),
+        vehicle: $("#vehicle").val(),
+      };
+      
+      // Sending information to the database
+      const result = await $.ajax({
+        url: "/api/maintenance",
+        type: "POST",
+        data: maint
+      })
+
+      // Re-directing user back to the maintenance page for the car
+      location.pathname = "/maintenance";
+    } catch (error) {
+      if(error) throw error;
+    } 
+  });
+}); //end doc ready
