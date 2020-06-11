8786667d2dd8447a68620b531680dad9fdedaed2
Ben K <ben.knight.brazil@hotmail.com>
2020-06-10 18:29:06 -0600

    started bulma, added image

--- models/vehicle.js
+++ models/vehicle.js
@@ -1,10 +1,10 @@
 // Dependencies
 // =============================================================
 
-// // This may be confusing but here Sequelize (capital) references the standard library
-// const Sequelize = require("sequelize");
-// // sequelize (lowercase) references our connection to the DB.
-// const sequelize = require("../config/config.json");
+// This may be confusing but here Sequelize (capital) references the standard library
+const Sequelize = require("sequelize");
+// sequelize (lowercase) references our connection to the DB.
+const sequelize = require("../config/config.json");
 
 module.exports = function(sequelize, DataTypes) {
   const Vehicle = sequelize.define("Vehicle", {
