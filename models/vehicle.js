8d671ca3ac6b1728d2666d607f80a26071a4eef4
Ben K <ben.knight.brazil@hotmail.com>
2020-06-10 18:57:08 -0600

    linter

--- models/vehicle.js
+++ models/vehicle.js
@@ -2,9 +2,9 @@
 // =============================================================
 
 // This may be confusing but here Sequelize (capital) references the standard library
-const Sequelize = require("sequelize");
+// const Sequelize = require("sequelize");
 // sequelize (lowercase) references our connection to the DB.
-const sequelize = require("../config/config.json");
+// const sequelize = require("../config/config.json");
 
 module.exports = function(sequelize, DataTypes) {
   const Vehicle = sequelize.define("Vehicle", {
